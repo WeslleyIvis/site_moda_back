154ebf8aae79bcc630626fba1e7e45880e037e84
William Cutrim da Silva <49375676+WilliamCutrim@users.noreply.github.com>
2021-09-20 23:51:48 -0300

    Primeira tabela criada. v1

---
+++ site_moda/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+
+    title           = models.TextField()
+    descriptions    = models.TextField()
+    price           = models.TextField()
--- site_moda/site_moda/settings.py
+++ site_moda/site_moda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+#from site_moda.products.models import Product
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'site_moda',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
